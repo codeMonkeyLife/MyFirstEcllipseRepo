e6973a1f3f4114f2f038ca3ec23dfaa37ff62618
xin <zexin511123@163.com>
2017-09-17 18:52:25 +0800

    在dev的分支中添加一行

--- src/Test.java
+++ src/Test.java
@@ -4,5 +4,6 @@ public class Test {
 		System.out.println("I am git project");
 		System.out.println("hahah");
 		System.out.println(123);
+		System.out.println("add a new line in dev branch");
 	}
 }
